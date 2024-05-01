257f80e6b93af32cc3b1b57387ac2fad9cce2e56
Federico Cogoni <federicocogoni90@gmail.com>
2024-05-01 19:32:08 +0200

    carousel responsive / search started / mini changes

--- src/components/MyList.jsx
+++ src/components/MyList.jsx
@@ -1,8 +1,9 @@
-import { Component } from "react"
-import { Carousel, Image, Spinner } from "react-bootstrap"
+import React, { Component } from "react"
+import { Spinner, Image } from "react-bootstrap"
+import Carousel from "react-multi-carousel"
 import "../assets/css/MyList.css"
+import "react-multi-carousel/lib/styles.css"
 
-// API_KEY = "b11caad1"
 const URL = "http://www.omdbapi.com/?apikey=b11caad1&s="
 
 class MyList extends Component {
@@ -22,6 +23,7 @@ class MyList extends Component {
         }
       })
       .then(data => {
+        console.log(data)
         this.setState({ search: data.Search, isLoading: false })
       })
       .catch(err => {
@@ -34,52 +36,50 @@ class MyList extends Component {
     this.fetchReservations(this.props.film)
   }
 
-  // soluzione senza carosello
-  // render() {
-  //   const { search, isLoading } = this.state
-  //   return (
-  //     <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-6 mb-4 d-flex justify-content-center align-items-center">
-  //       {isLoading ? (
-  //         <div className="col text-center">
-  //           <Spinner animation="grow" variant="light" role="status">
-  //             <span className="visually-hidden m-1">Loading...</span>
-  //           </Spinner>
-  //         </div>
-  //       ) : search.length > 0 ? (
-  //         search.slice(0, 6).map(film => (
-  //           <div className="col mb-2 mt-2 text-center" key={film.imdbID}>
-  //             <Image className="img-fluid cover" src={film.Poster} alt="poster" />
-  //           </div>
-  //         ))
-  //       ) : (
-  //         <div className="col">
-  //           <p>No films found</p>
-  //         </div>
-  //       )}
-  //     </div>
-  //   )
-  // }
-
   render() {
     const { search, isLoading } = this.state
+
+    const responsive = {
+      extraLargeDesktop: {
+        breakpoint: { max: 4000, min: 2001 },
+        items: 6,
+      },
+      largeDesktop: {
+        breakpoint: { max: 2000, min: 1400 },
+        items: 4,
+      },
+      desktop: {
+        breakpoint: { max: 1399, min: 992 },
+        items: 3,
+      },
+      tablet: {
+        breakpoint: { max: 991, min: 768 },
+        items: 2,
+      },
+      mobile: {
+        breakpoint: { max: 767, min: 0 },
+        items: 1,
+      },
+    }
+
     return (
       <div className="text-center">
         {isLoading ? (
-          <Spinner animation="border" variant="danger" role="status"></Spinner>
-        ) : search.length > 0 ? (
-          <Carousel>
-            {search.map(film => (
-              <Carousel.Item key={film.imdbID}>
+          <Spinner animation="border" variant="danger" role="status" />
+        ) : (
+          <Carousel responsive={responsive}>
+            {search.map(film => {
+              console.log(film)
+              return (
                 <Image
-                  className="d-block carouselImage me-auto ms-auto"
+                  key={film.imdbID}
+                  className="carouselImage"
                   src={film.Poster}
                   alt="film poster"
                 />
-              </Carousel.Item>
-            ))}
+              )
+            })}
           </Carousel>
-        ) : (
-          <p>No films found</p>
         )}
       </div>
     )
@@ -87,3 +87,29 @@ class MyList extends Component {
 }
 
 export default MyList
+
+// soluzione senza carosello
+// render() {
+//   const { search, isLoading } = this.state
+//   return (
+//     <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-6 mb-4 d-flex justify-content-center align-items-center">
+//       {isLoading ? (
+//         <div className="col text-center">
+//           <Spinner animation="grow" variant="light" role="status">
+//             <span className="visually-hidden m-1">Loading...</span>
+//           </Spinner>
+//         </div>
+//       ) : search.length > 0 ? (
+//         search.slice(0, 6).map(film => (
+//           <div className="col mb-2 mt-2 text-center" key={film.imdbID}>
+//             <Image className="img-fluid cover" src={film.Poster} alt="poster" />
+//           </div>
+//         ))
+//       ) : (
+//         <div className="col">
+//           <p>No films found</p>
+//         </div>
+//       )}
+//     </div>
+//   )
+// }
